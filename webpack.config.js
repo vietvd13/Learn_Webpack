e70070cbbddd014c6de9cd645c4e4a087aa3b025
vietvd13 <vuducviet0131@gmail.com>
2021-11-17 10:50:49 +0700

    Add webpack babel loader

--- webpack.config.js
+++ webpack.config.js
@@ -24,4 +24,18 @@ module.exports = {
       '@': path.resolve(__dirname, 'src/'),
     },
   },
+  module: {
+    rules: [
+      {
+        test: /\.m?js$/,
+        exclude: /(node_modules|bower_components)/,
+        use: {
+          loader: 'babel-loader',
+          options: {
+            presets: ['@babel/preset-env']
+          }
+        }
+      }
+    ]
+  }
 }
